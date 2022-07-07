716e62a6741b0bc51a3f2aa536efffa837a9da29
zscauer <zscauer@gmail.com>
2022-07-07 10:58:19 +0300

    api endpoints

--- binancetestbot/src/main/java/ru/tyumentsev/binancetestbot/controller/TestController.java
+++ binancetestbot/src/main/java/ru/tyumentsev/binancetestbot/controller/TestController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,13 +15,8 @@ import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.BinanceApiWebSocketClient;
 import com.binance.api.client.domain.account.Account;
 import com.binance.api.client.domain.account.AssetBalance;
-import com.binance.api.client.domain.event.AccountUpdateEvent;
-import com.binance.api.client.domain.event.AggTradeEvent;
 import com.binance.api.client.domain.event.BookTickerEvent;
 import com.binance.api.client.domain.event.CandlestickEvent;
-import com.binance.api.client.domain.event.DepthEvent;
-import com.binance.api.client.domain.event.OrderTradeUpdateEvent;
-import com.binance.api.client.domain.event.UserDataUpdateEvent.UserDataUpdateEventType;
 import com.binance.api.client.domain.market.CandlestickInterval;
 import com.binance.api.client.domain.market.OrderBook;
 import com.binance.api.client.domain.market.OrderBookEntry;
@@ -51,85 +47,74 @@ public class TestController {
             if (openedWebSocket == null) {
                 System.out.println("WebSocket is null");
             } else {
-                System.out.println(openedWebSocket.toString());
-            openedWebSocket.close();
+                System.out.println("Closing web socket " + openedWebSocket.toString());
+                openedWebSocket.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    @GetMapping("/test1")
-    public List<AssetBalance> testMethod1() {
+
+    @GetMapping("/accountbalance")
+    public List<AssetBalance> accountBalance() {
         Account account = restClient.getAccount();
         return account.getBalances();
     }
 
-    @GetMapping("/test2")
-    public void testMethod2() {
-        String listenKey = restClient.startUserDataStream();
+    @GetMapping("/accountbalance/{ticker}")
+    public AssetBalance assetBalance(@PathVariable String ticker) {
+        Account account = restClient.getAccount();
+        return account.getAssetBalance(ticker.toUpperCase());
+    }
+
+    @GetMapping("/orderbook/{pair}/asks")
+    public OrderBookEntry orderBookAsks(@PathVariable String pair) { // get orders
+        OrderBook orderBook = restClient.getOrderBook(pair.toUpperCase(), 10);
+        List<OrderBookEntry> asks = orderBook.getAsks();
+        OrderBookEntry firstAskEntry = asks.get(0);
+        System.out.println(firstAskEntry.getPrice() + " / " + firstAskEntry.getQty());
+
+        return firstAskEntry;
+    }
+
+    @GetMapping("/orderbook/{pair}/bids")
+    public OrderBookEntry orderBookBids(@PathVariable String pair) { // get orders
+        OrderBook orderBook = restClient.getOrderBook(pair.toUpperCase(), 10);
+        List<OrderBookEntry> bids = orderBook.getBids();
+        OrderBookEntry firstBidEntry = bids.get(0);
+        System.out.println(firstBidEntry.getPrice() + " / " + firstBidEntry.getQty());
+
+        return firstBidEntry;
+    }
+
+    @GetMapping("/booktickerevent/{pair}")
+    public void printBookTickerEvents(@PathVariable String pair) {
+        closeWebSocket();
         
-        webSocketClient.onUserDataUpdateEvent(listenKey, response -> {
-            if (response.getEventType() == UserDataUpdateEventType.ACCOUNT_POSITION_UPDATE) {
-                AccountUpdateEvent accountUpdateEvent = response.getOutboundAccountPositionUpdateEvent();
-                // Print new balances of every available asset
-                System.out.println(accountUpdateEvent.getBalances());
-            } else {
-                OrderTradeUpdateEvent orderTradeUpdateEvent = response.getOrderTradeUpdateEvent();
-                // Print details about an order/trade
-                System.out.println(orderTradeUpdateEvent);
-
-                // Print original quantity
-                System.out.println(orderTradeUpdateEvent.getOriginalQuantity());
-
-                // Or price
-                System.out.println(orderTradeUpdateEvent.getPrice());
-            }
+        openedWebSocket = webSocketClient.onBookTickerEvent(pair.toLowerCase(), (BookTickerEvent response) -> {
+            System.out.println("Ask price " + response.getAskPrice() + " / " + "Bid price " + response.getBidPrice());
         });
-        System.out.println("Waiting for events...");
     }
 
-    @GetMapping("/test3")
-    public void testMethod3() {
-        // String listenKey = restClient.startUserDataStream();
+    @GetMapping("/candlestickevent/{pair}")
+    public void printCandleStickEvents(@PathVariable String pair) {
+        closeWebSocket();
         
-       OrderBook orderBook = restClient.getOrderBook("BTCUSDT", 20);
-       List<OrderBookEntry> asks = orderBook.getAsks();
-       System.out.println(asks);
+        openedWebSocket = webSocketClient.onCandlestickEvent(pair.toLowerCase(), CandlestickInterval.ONE_MINUTE,
+                new BinanceApiCallback<CandlestickEvent>() {
+                    @Override
+                    public void onResponse(CandlestickEvent response) {
+                        System.out.println(response);
+                    }
+
+                    @Override
+                    public void onFailure(Throwable cause) {
+                        System.err.println("web socket failure");
+                        cause.printStackTrace(System.err);
+                    }
+                });
     }
 
-    @GetMapping("/test4")
-    public void testMethod4() {
-        // String listenKey = restClient.startUserDataStream();
-        
-        webSocketClient.onBookTickerEvent("BTCUSDT", (BookTickerEvent resp) -> {
-            System.out.println(resp.getAskPrice() + resp.getBidPrice());
-       });
-    }
-
-    @GetMapping("/test5")
-    public void testMethod5() {
-        // String listenKey = restClient.startUserDataStream();
-        System.out.println("in test 5...");
-        openedWebSocket = webSocketClient.onCandlestickEvent("ethbtc", CandlestickInterval.ONE_MINUTE, new BinanceApiCallback<CandlestickEvent>() {
-
-
-            @Override
-            public void onFailure(Throwable cause) {
-                // TODO Auto-generated method stub
-                System.err.println("web socket failure");
-                cause.printStackTrace(System.err);
-            }
-
-            @Override
-            public void onResponse(CandlestickEvent response) {
-                System.out.println(response);
-                
-            }
-            
-        });
-
-    }
-
+    
 
 }
